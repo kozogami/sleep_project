455fdc497050da00e471b25d7a0ceb140eafb8c8
kozogami <153994577+kozogami@users.noreply.github.com>
2024-11-04 23:26:38 -0600

    improved fade-in

--- Assets/Script/PlayerBehaviourScript.cs
+++ Assets/Script/PlayerBehaviourScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 
@@ -15,6 +16,8 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject LightControlF;
     public GameObject locker;
     public GameObject endLock;
+    public AudioMixer mixer;
+    
 
 
     bool music;
@@ -106,10 +109,11 @@ public class NewBehaviourScript : MonoBehaviour
 
             angle += increment * 1 * Time.deltaTime; // Angle increment, with a fixed time.
 
-            //call fadeAudio class to (hopefully) smoothly transition between lower and higher volumes
-            //it takes the arguments (audio source, duration of transition, target volume)
-            StartCoroutine(FadeAudioSource.StartFade(mainAudio, 0.75f, 1.0f));
-            StartCoroutine(FadeAudioSource.StartFade(secondaryAudio, 0.75f, 1.0f));
+            //call fadeAudioMixer class to (hopefully) smoothly transition between lower and higher volumes
+            //it takes the arguments (audioMixer, exposedVolumeParameter, duration of transition, target volume)
+            
+            StartCoroutine(FadeMixerGroup.StartFade(mixer, "subVol", 0.75f, 0f));
+           
 
             //Debug.Log("Current value: " + angle);
         }
@@ -122,8 +126,8 @@ public class NewBehaviourScript : MonoBehaviour
 
             angle -= increment * 1 * Time.deltaTime;
 
-            if (music) StartCoroutine(FadeAudioSource.StartFade(mainAudio, 0.75f, 0.25f));
-            if (music2) StartCoroutine(FadeAudioSource.StartFade(secondaryAudio, 0.75f, 0.25f));
+            if (music2) StartCoroutine(FadeMixerGroup.StartFade(mixer, "subVol", 0.75f, 0.25f));
+
         }
 
         animator.SetFloat("Position", angle);// update the angle to the unity
